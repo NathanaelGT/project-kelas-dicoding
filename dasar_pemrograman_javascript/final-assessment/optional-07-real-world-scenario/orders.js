ac1655a2a4888caa41bb3bd380e0ee9e00f76140
NathanaelGT <gordontanu2004@gmail.com>
2024-10-23 17:23:56 +0800

    07: Real world scenario

--- dasar_pemrograman_javascript/final-assessment/optional-07-real-world-scenario/orders.js
+++ dasar_pemrograman_javascript/final-assessment/optional-07-real-world-scenario/orders.js
@@ -1,22 +1,78 @@
-// Gunakan fungsi di bawah ini untuk menghasilkan id yang unik
+/**
+ * Gunakan fungsi di bawah ini untuk menghasilkan id yang unik
+ *
+ * @returns {string}
+ */
 function generateUniqueId() {
   return `_${Math.random().toString(36).slice(2, 9)}`;
 }
 
+/**
+ * @typedef {Object} Item
+ * @property {string} name
+ * @property {number} price
+ */
 
-// TODO: buatlah variabel yang menampung data orders
-let orders;
+/**
+ * @typedef {'Menunggu' | 'Diproses' | 'Selesai'} Status
+ */
 
-// TODO: selesaikan fungsi addOrder
-function addOrder(customerName, items) {}
+/**
+ * @typedef {Object} Order
+ * @property {string} id
+ * @property {string} customerName
+ * @property {Item[]} items
+ * @property {number} totalPrice
+ * @property {Status} status
+ */
 
-// TODO: selesaikan fungsi updateOrderStatus
-function updateOrderStatus(orderId, status) {}
+/**
+ * @type {Order[]}
+ */
+const orders = [];
 
-// TODO: selesaikan fungsi calculateTotalRevenue dari order yang berstatus Selesai
-function calculateTotalRevenue() {}
+/**
+ * @param {string} customerName
+ * @param {Item[]} items
+ */
+function addOrder(customerName, items) {
+  orders.push({
+    id: generateUniqueId(),
+    customerName,
+    items,
+    totalPrice: items.reduce((totalPrice, item) => totalPrice + item.price, 0),
+    status: 'Menunggu',
+  });
+}
 
-// TODO: selesaikan fungsi deleteOrder
-function deleteOrder(id) {}
+/**
+ * @param {string} orderId
+ * @param {Status} status
+ */
+function updateOrderStatus(orderId, status) {
+  const order = orders.find(order => order.id === orderId);
+  if (order) {
+    order.status = status;
+  }
+}
+
+/**
+ * @returns {number}
+ */
+function calculateTotalRevenue() {
+  return orders.reduce((totalRevenue, order) => {
+    return totalRevenue + Number(order.status === 'Selesai') * order.totalPrice;
+  }, 0);
+}
+
+/**
+ * @param {string} id
+ */
+function deleteOrder(id) {
+  orders.splice(
+    orders.findIndex(order => order.id === id),
+    1,
+  );
+}
 
 export { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder };
