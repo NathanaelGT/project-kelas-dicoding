9fc884445a44820bca10321f88c2e0006fc2f18e
NathanaelGT <gordontanu2004@gmail.com>
2024-10-23 16:53:56 +0800

    05: Recursive

--- dasar_pemrograman_javascript/final-assessment/optional-05-recursive/fibonacci.js
+++ dasar_pemrograman_javascript/final-assessment/optional-05-recursive/fibonacci.js
@@ -1,5 +1,16 @@
+/**
+ * @param {number} n
+ * @returns {number[]}
+ */
 function fibonacci(n) {
+  if (n === 0) return [0];
 
+  const result = fibonacci(n - 1);
+  const next = n === 1 ? 1 : result[n - 1] + result[n - 2];
+
+  result.push(next);
+
+  return result;
 }
 
 // Jangan hapus kode di bawah ini!
